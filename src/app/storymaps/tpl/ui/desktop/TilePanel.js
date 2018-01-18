0cc6e2f65ff38735bfd1eb815750ba2b6a7379d8
scMarth <vlantaca@gmail.com>
2018-01-18 11:31:03 -0800

    fixed padding in tilelist

--- src/app/storymaps/tpl/ui/desktop/TilePanel.js
+++ src/app/storymaps/tpl/ui/desktop/TilePanel.js
@@ -31,8 +31,9 @@ define(["esri/geometry/screenUtils",
 			this.resize = function(newWidth)
 			{
 				$("#paneLeft").width(newWidth);
-				$(".tilelist").width(newWidth - app.cfg.MYLIST_MARGIN);
+				$(".tilelist").width(newWidth - (app.cfg.MYLIST_MARGIN*2));
 				$(".tilelist").css('padding-left', app.cfg.MYLIST_MARGIN);
+				$(".tilelist").css('padding-right', app.cfg.MYLIST_MARGIN);
 			};
 
 			this.createTab = function(index, layer){
