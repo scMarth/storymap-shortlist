2cf3948d21e77e80d9dfe9d9b7354936b37d390f
scMarth <vlantaca@gmail.com>
2018-02-09 10:56:52 -0800

    minor style changes

--- src/app/custom-scripts.js
+++ src/app/custom-scripts.js
@@ -2,6 +2,9 @@ define(["dojo/topic"], function(topic) {
 	/*
 	* Custom Javascript to be executed while the application is initializing goes here
 	*/
+   $('.logoLink').attr('href', 'https://www.cityofsalinas.org/');
+                  // logoLink.css('cursor', 'pointer')
+                  //             .attr('href', headerCfg.logoTarget);
 
 	// The application is ready
 	topic.subscribe("tpl-ready", function(){
